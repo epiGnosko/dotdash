f5d68c6b2fc39e1ed3b39e81b147cb4bf2ef0fb9
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2025-02-22 13:00:22 +0530

    corrected rookie mistake

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 mkdir -p /usr/local/lib/python3.x/
 
+rm -rf /usr/local/lib/python3.x/dotdash
+
 cp -r dotdash/ /usr/local/lib/python3.x/dotdash/
 
-ln -s /usr/local/lib/python3.x/dotdash/main.py /usr/local/bin/dotdash
+ln -sf /usr/local/lib/python3.x/dotdash/main.py /usr/local/bin/dotdash
 
 chmod +x /usr/local/lib/python3.x/dotdash/main.py
