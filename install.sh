e0cf292b70f4cea20e97ffe8a024daebab73f09a
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2025-03-10 13:51:30 +0530

    Started Refactor to Rust

--- install.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p /usr/local/lib/python3.x/
-
-rm -rf /usr/local/lib/python3.x/dotdash
-
-cp -r dotdash/ /usr/local/lib/python3.x/dotdash/
-
-ln -sf /usr/local/lib/python3.x/dotdash/main.py /usr/local/bin/dotdash
-
-chmod +x /usr/local/lib/python3.x/dotdash/main.py
--- uninstall.sh
+++
@@ -1,2 +0,0 @@
-rm /usr/local/bin/dotdash
-rm -rf /usr/local/lib/python3.x/dotdash
